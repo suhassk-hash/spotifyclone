eae6e90a7d12a6f3c84aee475882bab248e01575
suhassk-hash <sharmasuhas450@gmail.com>
2024-09-16 06:04:23 +0530

    player

--- src/app/(components)/VideoPlayer.tsx
+++ src/app/(components)/VideoPlayer.tsx
@@ -160,10 +160,4 @@ const MusicPlayer: React.FC = () => {
     );
 };
 
-const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
-};
-
 export default MusicPlayer;
